d5dde4c68a75010ef56ff1b7359b937cbc9a149f
abdul-hade <ah5041430@gmail.com>
2025-04-20 04:24:28 +0500

    Initial commit

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
-  console.log(`✅ Server running at http://localhost:${port}`);
+  console.log(`Dummy app running at http://localhost:${port}`);
 });
+
+  
